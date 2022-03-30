aa49276dd2c351554b6ed96cc1a6c4c96460667a
Matthew Pak <mattpak@gmail.com>
2022-03-30 15:54:13 -0700

    Set up webpack+Babel with dev scripts in package.json

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+require('dotenv').config();
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: '/client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
