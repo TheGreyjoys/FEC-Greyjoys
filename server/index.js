c88029372836ea62e4f3db44121dd29a3e82bd7f
Kieran <kieran.yun@gmail.com>
2022-03-31 12:45:40 -0700

    server now working with template literals, removed 'next', using destructuring

--- server/index.js
+++ server/index.js
@@ -18,20 +18,22 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.all('*', (req, res) => {
   const config = { headers: { authorization: process.env.TOKEN } };
   const { method, url, body } = req;
+  // const target = path.join('http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp', url);
   if (method === 'GET') {
-    axios.get(path.join('http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp', url), config)
+    // console.log(target);
+    axios.get(`http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${url}`, config)
       .then((response) => {
         res.send(response.data);
       })
       .catch((err) => res.send(err));
   } else if (method === 'PUT') {
-    axios.put(path.join('http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp', url), config)
+    axios.put(`http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${url}`, config)
       .then((response) => {
         res.send(response);
       })
       .catch((err) => res.send(err));
   } else {
-    axios.post(path.join('http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp', url), body, config)
+    axios.post(`http://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp${url}`, body, config)
       .then((response) => {
         res.send(response);
       })
