f34ccf3d542ad62fa8f75eefcb37ff9fd341d5a1
Matthew Pak <mattpak@gmail.com>
2022-03-30 17:18:43 -0700

    Implemented Airbnb linter config and updated files to comply

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = process.env.PORT || 3000
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/', (req, res, next) => {
   console.log(`${req.method} at ${req.url}`);
   next();
-})
+});
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
