10884bf10e06d8a5c876ba72de504c20da9dd6d5
Kieran <kieran.yun@gmail.com>
2022-03-30 18:05:53 -0700

    wrote out a middleware function in the server that will forward all HTTP requests to the API, it is not tested yet

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const axios = require('axios')
 const port = process.env.PORT || 3000
 
 app.use('/', (req, res, next) => {
@@ -9,6 +10,17 @@ app.use('/', (req, res, next) => {
 })
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//a route that will forward all incoming HTTP requests to the API
+app.all('*', (req, res, next) => {
+  let config = {headers: {authorization: process.env.TOKEN}};
+  let method = req.method;
+  if (req.method === 'GET' || req.methods === 'PUT') {
+    axios.[method](path.join('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/', req.url), config) //method might have to be in brackets
+  } else {
+    axios.[method](path.join('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/', req.url), req.body, config)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
