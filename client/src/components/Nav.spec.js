c50cd4b2c5b0ef476aad1addc41d299da9eda539
Clover Hong <jiaxun.hong@gmail.com>
2022-03-31 19:03:00 -0700

    Got rid of some comments and console.logs

--- client/src/components/Nav.spec.js
+++ client/src/components/Nav.spec.js
@@ -10,9 +10,5 @@ import App from './app';
 test('renders nav bar on button click', () => {
   render(<App />);
   fireEvent.click(screen.getByRole('button'));
-  console.log(screen.getByText('Nav'));
   expect(screen.getByText('Search').toExist);
-/*
-fireEvent.click(screen.getByRole('button'));
-  expect(screen.getByRole('button', { value: 'Search' }).toExist); */
 });
