377f94255ffccdbd7b7e0f25bc9cb1bda9eebd96
Matthew Pak <mattpak@gmail.com>
2022-04-14 16:40:42 -0700

    Cleaned up comments in dataCache

--- client/src/dataCache.js
+++ client/src/dataCache.js
@@ -1,35 +1,5 @@
 const dataCache = {};
 
-/*
-goals:
--reduce reduntant api requests
--increase loading performance by reducing reliance on fresh api data for render
--input in chunks based on grouped API calls
-
-initialization:
-ProdDetail: currentProduct, productstyles, reviewMeta
-RelProd(Card): currentProduct, productStyles, reviewMeta
-reviews: reviews, reviewMeta
-
-concept:
--perform core initialization fetch (currentProduct, productStyles, reviewMeta) in app
--components will attempt fetch and reroute to cached data
--all subsequent fetches will cache newly fetched data
--EXTRA: cached data will timeout after x minutes
-
-execution:
--refactor all GET client request handlers to check cache if data exists already (by prodID)
-
-storage schema:
-
-prodId: {
-  info: {},
-  styles: {};
-  reviews: {};
-  reviewMeta: {};
-}
-*/
-
 const cache = (prodId, section, data) => {
   const prodCache = dataCache[prodId];
   if (!prodCache) {
